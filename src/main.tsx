4fb6fa921949fce256beefe542296a5efd64ad05
lbarallobre <lucas.barallobre@ortex.com>
2023-05-24 15:24:56 -0300

    Merge App into main

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './pages/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@/styles/globals.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { EpisodeDetail } from "@/pages/EpisodeDetail";
+import { Home } from "@/pages/Home";
+import { PodcastDetail } from "@/pages/PodcastDetail";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/podcast/:id">
+        <PodcastDetail />
+      </Route>
+      <Route path="/podcast/:id/episode/:episodeId">
+        <EpisodeDetail />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
