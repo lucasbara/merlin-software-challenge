581e5a61f55057c795bce94a51f193dc9e39f792
lbarallobre <lucas.barallobre@ortex.com>
2023-05-24 15:13:32 -0300

    Add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    colors: {},
+    colors: {
+      black: "#000000",
+      blue: "#4C7AA2",
+      grey: "#999999",
+      "light-grey": "#E6E6E6",
+      "secondary-grey": "#edf2f4",
+      white: "#FFFFFF",
+    },
   },
   plugins: [],
 };
